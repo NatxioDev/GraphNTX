e21671c8a4c1d45a6219ed473fd38f2977ce696a
Ignacio Paz <ignaciopaz72@gmail.com>
2021-04-22 19:34:28 -0400

    Antes de hacer una locura

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./scss/app.scss";
-import "../node_modules/bootstrap" 
+import "../node_modules/bootstrap";
 import "../node_modules/@fortawesome/fontawesome-free/js/brands.js";
 import "../node_modules/@fortawesome/fontawesome-free/js/solid.js";
 import "../node_modules/@fortawesome/fontawesome-free/js/fontawesome.js";
@@ -18,3 +18,38 @@ var firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
+var btnLogIn = document.getElementById("btnIniciarSesion");
+var btnLogInGgle = document.getElementById("btnIniciarGoogle");
+// btnIniciar = document.getElementById("btnIniciarSesion")
+btnLogIn.addEventListener("click", function () {
+  var email = document.getElementById("inputEmail").value;
+  var pass = document.getElementById("inputPass").value;
+  var user = { email: email, pass: pass };
+
+  verificarUsuario(user) ? iniciarSesion(user) : error("Error en los campos");
+});
+
+btnLogInGgle.addEventListener("click", function () {
+  console.log("Google");
+});
+
+function verificarUsuario(user) {
+  console.log(user);
+  if (user.email.trim().length > 0 && user.pass.trim().length > 0) return true;
+  return false;
+}
+function iniciarSesion(user) {
+  console.log(user);
+}
+
+function error(msg) {
+  alert("", msg);
+}
+
+function changeDir(url, tiempo) {
+  let actualPage = location.href;
+  console.log(actualPage);
+  setTimeout(function () {
+    location.href = url;
+  }, tiempo * 1000);
+}
